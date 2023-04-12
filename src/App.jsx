b3d4dc0ab7dad896b59e551fbc132c372206133d
Ajmera Pavan Kumar <ajmerapavan20@gmail.com>
2023-04-13 00:09:33 +0530

    Birthday-Lists

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import data from './data'
+import List from './List';
+
+function App() {
+  const [people, setPeople] = useState(data);
+  // console.log(people);
+  
+  return(
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button type='button' className='btn btn-block' onClick={()=>setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  )
+}
+
+export default App
